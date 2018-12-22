dbc4e87f91b4652f1efd52738240c9175ad3dfb8
huangzhi <huangz@g2l-service.com>
2018-12-22 16:21:21 +0800

    增加email功能

---
+++ src/main/java/com/demo/test/EmailTest.java
@@ -0,0 +1,90 @@
+package com.demo.test;
+
+import java.io.File;
+
+import javax.mail.internet.MimeMessage;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@ActiveProfiles("163")
+public class EmailTest {
+ 
+    @Autowired
+    private JavaMailSender mailSender; //自动注入的Bean
+ 
+    @Value("${spring.mail.username}")
+    private String Sender; //读取配置文件中的参数
+ 
+    /**
+     * 简单文本
+     * @throws Exception
+     */
+    @org.junit.Test
+    public void sendSimpleMail() throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(Sender);
+        message.setTo("43838422@qq.com"); 
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容");
+        mailSender.send(message);
+    }
+    
+    /**
+     * 发送Html邮件
+     */
+    @org.junit.Test
+    public void sendHtmlMail() {
+        MimeMessage message = null;
+        try {
+            message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(Sender);
+            helper.setTo(Sender);
+            helper.setTo("43838422@qq.com"); 
+    
+            StringBuffer sb = new StringBuffer();
+            sb.append("<h1>大标题-h1</h1>")
+                    .append("<p style='color:#F00'>红色字</p>")
+                    .append("<p style='text-align:right'>右对齐</p>");
+            helper.setText(sb.toString(), true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mailSender.send(message);
+    }
+    
+   /**
+    * 带附件的邮件
+    */
+    @org.junit.Test
+    public void sendAttachmentsMail() {
+        MimeMessage message = null;
+        try {
+            message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(Sender);
+            helper.setTo(Sender);
+            helper.setSubject("主题：带附件的邮件");
+            helper.setText("带附件的邮件内容");
+            //注意项目路径问题，自动补用项目路径
+            FileSystemResource file = new FileSystemResource(new File("E:\\艺术照\\第二套\\1.jpg"));
+            //加入邮件
+            helper.addAttachment("图片.jpg", file);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        mailSender.send(message);
+    }
+}
